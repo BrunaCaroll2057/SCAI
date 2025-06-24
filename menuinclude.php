ca849a850ebc7df3dd403bc679d34d73cca5d6c0
BrunaCaroll2057 <brunacarolinerodrigues4@gmail.com>
2025-06-24 13:35:39 -0300

    menu

--- menuinclude.php
+++ menuinclude.php
@@ -12,7 +12,7 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="principal.php">Home</a>
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="sobre.php">Sobre</a>
