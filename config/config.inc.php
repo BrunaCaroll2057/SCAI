5f59b625ec0f373f8be8024ca56597e3761c2cd0
BrunaCaroll2057 <brunacarolinerodrigues4@gmail.com>
2025-09-01 14:48:41 -0300

    detalhes

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,11 +1,7 @@
 <?php
 
 define('USUARIO', 'root'); /// usuario de conexão com o banco
-<<<<<<< HEAD
 define('SENHA', 'brun@c4ro!2057'); // senha de conexão com o banco
-=======
-define('SENHA', ''); // senha de conexão com o banco
->>>>>>> 9945479a0f73ebc0633068e4edcb6c6be0503c42
 define('HOST', 'localhost'); // ip do servidor do banco
 define('PORT', '3306'); // porta do mysql
 define('DB', 'scai'); // nome do banco
