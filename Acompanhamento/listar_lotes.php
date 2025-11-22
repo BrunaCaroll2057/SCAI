312679b9d6a98925376123c0fe213bffe43d786b
BrunaCaroll2057 <brunacarolinerodrigues4@gmail.com>
2025-11-22 14:36:48 -0300

    atualização

--- Acompanhamento/listar_lotes.php
+++ Acompanhamento/listar_lotes.php
@@ -1,15 +1,14 @@
 <?php
 // Inclusão dos arquivos necessários
 require_once("../Classes/Database.class.php");
-include_once '../Includes/menuinclude.php'; // Incluindo o menu, se necessário
-include_once '../config/config.inc.php'; // Incluindo a configuração de banco de dados
+include_once '../config/config.inc.php';
 
 // Consultar todos os lotes
-$sql_lotes = "SELECT id_lote, porca, lote, vivos, mortos, mumia FROM lotes ORDER BY id_lote ASC"; // Mudei para ASC para mostrar em ordem crescente
+$sql_lotes = "SELECT id_lote, porca, lote, vivos, mortos, mumia FROM lotes ORDER BY id_lote ASC";
 $lotes_result = Database::consultar($sql_lotes);
 
 if ($lotes_result === false) {
-    die("Erro na consulta dos lotes: " . Database::getLastError()); // Assumindo que Database tem um método para erro
+    die("Erro na consulta dos lotes: " . Database::getLastError());
 }
 
 $lotes = $lotes_result->fetchAll(PDO::FETCH_ASSOC);
@@ -20,8 +19,13 @@ $lotes = $lotes_result->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Lista de Lotes</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>SCAI - Sistema de Coordenação de Animais do Instituto</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="JS/js.js"> </script>
+    <link rel="stylesheet" href="css/estilo.css">
     <script>
         function confirmarExclusao(id) {
             if (confirm("Tem certeza que deseja excluir este lote? Isso também excluirá todos os leitões associados e reordenará os IDs dos lotes restantes.")) {
@@ -32,11 +36,9 @@ $lotes = $lotes_result->fetchAll(PDO::FETCH_ASSOC);
 
 </head>
 <body>
-
-        <style>
-             
-
-        </style>
+<?php 
+   include __DIR__ . '/Includes/menuinclude.php';
+?>
 
 <div class="container my-5">
     <h3 class="mb-3">Lista de Lotes de Porca</h3>
